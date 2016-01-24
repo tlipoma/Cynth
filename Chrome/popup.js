386d8cd1acc45a7815a8d6e847db0075d3347fe9
tlipoma <tlipoma@restdevices.com>
2016-01-24 09:15:31 -0500

    basic extension that polls flask server for single username/password then displays it on screen

--- Chrome/popup.js
+++ Chrome/popup.js
@@ -36,73 +36,26 @@ function getCurrentTabUrl(callback) {
 
     callback(url);
   });
-
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, function(tabs) {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
 }
 
-/**
- * @param {string} searchTerm - Search term for Google Image search.
- * @param {function(string,number,number)} callback - Called when an image has
- *   been found. The callback gets the URL, width and height of the image.
- * @param {function(string)} errorCallback - Called when the image is not found.
- *   The callback gets a string that describes the failure reason.
- */
-function getImageUrl(searchTerm, callback, errorCallback) {
-  // Google image search - 100 searches per day.
-  // https://developers.google.com/image-search/
-  var searchUrl = 'https://ajax.googleapis.com/ajax/services/search/images' +
-    '?v=1.0&q=' + encodeURIComponent(searchTerm);
-  var x = new XMLHttpRequest();
-  x.open('GET', searchUrl);
-  // The Google image search API responds with JSON, so let Chrome parse it.
-  x.responseType = 'json';
-  x.onload = function() {
-    // Parse and process the response from Google Image Search.
-    var response = x.response;
-    if (!response || !response.responseData || !response.responseData.results ||
-        response.responseData.results.length === 0) {
-      errorCallback('No response from Google Image search!');
-      return;
-    }
-    var firstResult = response.responseData.results[0];
-    // Take the thumbnail instead of the full image to get an approximately
-    // consistent image size.
-    var imageUrl = firstResult.tbUrl;
-    var width = parseInt(firstResult.tbWidth);
-    var height = parseInt(firstResult.tbHeight);
-    console.assert(
-        typeof imageUrl == 'string' && !isNaN(width) && !isNaN(height),
-        'Unexpected respose from the Google Image Search API!');
-    callback(imageUrl, width, height);
-  };
-  x.onerror = function() {
-    errorCallback('Network error.');
-  };
-  x.send();
-}
 
 function getPasswordFromServer(currentUrl, callback, errorCallback){
   var serverIP = '104.236.10.146:5000/';
   var url = 'http://' + serverIP;
+  url = 'http://104.236.10.146:5000/';
   var req = new XMLHttpRequest();
   req.open('GET', url);
   req.responseType = 'json';
   req.onload = function(){
-    // Check for errors
-    if (!response || !response.responseData || !response.responseData.results ||
-        response.responseData.results.length === 0) {
-      errorCallback('No response from Server!!!');
-      return;
-    }
     var response = req.response;
-    callback(response);
+
+    // Check for errors
+    //if (!response || !response.responseData || !response.responseData.results ||
+    //    response.responseData.results.length === 0) {
+    //  errorCallback('No response from Server!!!');
+    //  return;
+    //}
+    callback("user:"+response.username + "  password:"+response.username);
   };
   req.onerror = function() {
     var status = req.statusText;
