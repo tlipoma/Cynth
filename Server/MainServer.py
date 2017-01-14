194b8727b8e9c4b40b46771bed5dfad2cf46a7c3
tlipoma <tlipoma@gmail.com>
2017-01-13 19:45:38 -0500

    moved save pass over to mlab

--- Server/MainServer.py
+++ Server/MainServer.py
@@ -13,6 +13,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Open mongodb connection
+logger.info('Initilizing DB connection')
 try:
 	MLAB_URI = os.environ['CYNTH_MLAB_URI']
 except:
@@ -26,25 +27,35 @@ except:
 def get_from_mlab(filename):
 	return password_db.find_one({'uri':filename})
 
+def save_to_mlab(filename, data):
+	try:
+		password_db.update({'uri':filename}, 
+			{'uri':filename, 'data':data},
+			upsert=True)
+		return True
+	except:
+		logger.info('Error saving data for ' + filename)
+		return False
+
 @app.route('/store', methods=['POST', 'GET'])
 def storePassword():
 	desiredFileName = request.json['file']
 	encryptedData = request.json['data']
 	# Save file to disk
-	file = open("store/"+desiredFileName+".cynth", "w")
-	file.write(encryptedData)
-	file.close()
-	return "completed"
+	if save_to_mlab(desiredFileName, encryptedData):
+		return "completed"
+	return "error saving"
 
 @app.route('/getPassword/<filename>')
 def getPassword(filename):
+	logger.info('Getting password data for ' + filename)
 	desiredFileName = filename
 	# get data from mongo
 	password_data = get_from_mlab(filename)
 	data = {}
 	data['file'] = desiredFileName
 	if password_data:
-		data['encryptedData'] = password_data['password']
+		data['encryptedData'] = password_data['data']
 	else:
 		data['encryptedData'] = 'no password'
 	return json.dumps(data)
